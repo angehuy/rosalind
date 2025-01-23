98009bf0420cc7bcb63519ccd3aa55ed0c96784f
Angela T. Huynh <ahuynh5107@gmail.com>
2025-01-23 16:50:52 -0500

    Separated into 2 functions for versatility

--- ROSALIND_GCcontent.py
+++ ROSALIND_GCcontent.py
@@ -14,18 +14,19 @@ TGGGAACCTGCGGGCAGTAGGTGGAAT
 '''
 
 
-def format_fasta(input_file):
+# readSequence function creates a dictionary of accession IDs to sequence data
+# computeGC function prints the accession ID of the sequence with the highest GC content and its percentage
+
+def readSequence(input_file):
     seq = ''
     acc_seq = {}
     
-    with open(input_file, 'r') as infile:
-        for line in infile:
+    with open(input_file, 'r') as f:
+        for line in f:
             line = line.strip()
-            if line.startswith(">"):  # If it's an accession line
+            if line[0] == ">": # If it's an accession line
                 if seq:  # If sequence collected from previous run, collect it
-                    GC_count = seq.count("G") + seq.count("C")
-                    GC_per = GC_count / len(seq)
-                    acc_seq[id] = [GC_count, GC_per]
+                    acc_seq[id] = seq
 
 
                 id = line[1:] # id stored in this line
@@ -34,25 +35,38 @@ def format_fasta(input_file):
                 seq += line  # Append the sequence data to the current sequence
 
         if seq: # For the very last line since there's no ">" to complete the GC calculations & dictionary adding
-            GC_count = seq.count("G") + seq.count("C")
-            GC_per = (GC_count / len(seq)) * 100
-            acc_seq[id] = [GC_count, GC_per]
+            acc_seq[id] = seq
 
-        max_GC =  max(acc_seq, key=lambda k: acc_seq[k][0])
-        max_GC_per = acc_seq[max_GC][1]
-        print(max_GC)
-        print(max_GC_per)
+        # print(type(acc_seq))
+        return acc_seq
+
+def computeGC(input_file):
+    GC_dict = readSequence(input_file)
+
+    for key in list(GC_dict.keys()): # can't directly iterate over GC_dict bc while iterating, python expects stable dict size
+        seq = GC_dict[key]
+        GC_count = seq.count("G") + seq.count("C")
+        GC_per = (GC_count / len(seq)) * 100
+        GC_dict[key] = [GC_count, GC_per]
+    
+    # Now, output the ID and the GC percent of the sequence that has the highest GC count
+    max_GC =  max(GC_dict, key=lambda k: GC_dict[k][0])
+    max_GC_per = GC_dict[max_GC][1]
+    print(max_GC)
+    print(max_GC_per)
+
+
+
 
 
 if __name__ == "__main__" :
     input_file = 'GC_file.txt'
-    format_fasta(input_file)
+    computeGC(input_file)
 
 
-''' What I learned
+
+''' Learning Notes:
 - due to mult-line structure of the sequences, pay attention to the structure
 of code and how I appended the multi-line of the sequences in the same iteration the > accession line was detected
 
 '''
-
-
