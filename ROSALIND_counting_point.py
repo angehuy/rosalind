8053c57056674e2ee12131498b08746820ce27c9
Angela T. Huynh <ahuynh5107@gmail.com>
2025-02-16 16:40:33 -0500

    adding sol

---
+++ ROSALIND_counting_point.py
@@ -0,0 +1,26 @@
+
+# Assume sequences are of equal lengths
+
+
+def count_mut(seq1, seq2):
+    count = 0
+    l_1 = len(seq1)
+    for i in l_1:
+        if seq1[i] != seq2[i]:
+            count += 1
+
+def count_mut2(seq1, seq2):
+    count = 0
+    for a,b in zip(seq1, seq2):
+        if a!= b:
+            count += 1
+
+count_mut2("bch", "bcb")
+
+
+'''
+- zip uses lazy evaluation (returns iterator); values needed are not computed until they're used (i.e. when you iterate over them)
+- for loop with indexing requires access to full list at once, so all the elements are already stored in memory
+
+
+'''
